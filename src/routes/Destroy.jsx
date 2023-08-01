6405d2e8e730def67a83e259a0c27d9200e476bc
CESCO <cescovalledev@gmail.com>
2023-08-01 16:14:48 -0600

    delete contact refreshing done

--- src/routes/Destroy.jsx
+++ src/routes/Destroy.jsx
@@ -2,6 +2,6 @@ import { redirect } from "react-router-dom";
 import { deleteContact } from "./contacts";
 
 export async function action({ params }) {
-  deleteContact(params.contactId);
+  await deleteContact(params.contactId);
   return redirect("/");
 }
